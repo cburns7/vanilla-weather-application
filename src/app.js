6267f7cdec96aff29ec01f856ad8a1f9b7f75b9f
cburns7 <80853076+cburns7@users.noreply.github.com>
2021-04-03 19:31:19 -0400

    added search engine

--- src/app.js
+++ src/app.js
@@ -42,7 +42,17 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "86efc84703d8920edad71687828e1265";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "86efc84703d8920edad71687828e1265";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
