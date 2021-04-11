8dd364f2ab9e9c43bdea74892be2f1afdb6504bc
cburns7 <80853076+cburns7@users.noreply.github.com>
2021-04-11 14:53:32 -0400

    display forecast data using the api

--- src/app.js
+++ src/app.js
@@ -21,28 +21,39 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
   <div class="col-2">
-    <div class="weather-forecast-date">${day}</div>
-      <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="" width="70"/>
+    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+      <img src="http://openweathermap.org/img/wn/${
+        forecastDay.weather[0].icon
+      }@2x.png" alt="" width="70"/>
     <div class="weather-forecast-temperatures"> 
       <span class="weather-forecast-temperatures-max">
-        18°
+        ${Math.round(forecastDay.temp.max)}°
       </span>
       <span class="weather-forecast-temperatures-min"> 
-        12°
+      ${Math.round(forecastDay.temp.min)}°
       </span>
     </div>
   </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -53,7 +64,7 @@ function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "86efc84703d8920edad71687828e1265";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiURL).then(displayForecast);
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
